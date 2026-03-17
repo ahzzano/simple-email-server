f0703834afe51fb76a47f1d182c0d3c90dada617
W. Santos <w.santos.gg@gmail.com>
2026-03-17 23:48:37 +0800

    feat: add basic exchange

--- src/session.rs
+++ src/session.rs
@@ -1,12 +1,33 @@
-use tokio::{io::{AsyncWrite, AsyncWriteExt, BufReader}, net::{TcpStream, tcp::WriteHalf}};
+use tokio::{io::{AsyncBufReadExt, AsyncWrite, AsyncWriteExt, BufReader}, net::{TcpStream, tcp::WriteHalf}};
+
+
+enum Commands {
+    Helo,
+    Ehlo,
+    Rcpt,
+    Mail,
+    Data,
+    Quit,
+}
+
+enum SessionState { 
+    Created,
+    Connected,
+    MailFrom(String),
+    RcptTo(String, String),
+    Data(String, String),
+}
 
 pub struct Session {
     stream: TcpStream,
     hostname: String,
+    state: SessionState
 }
 
-enum SessionState { 
-    Connected
+pub struct Mail {
+    sender: String,
+    reciever: String,
+    body: String,
 }
 
 async fn send<W>(writer: &mut W, cmd:&str) 
@@ -19,7 +40,7 @@ where
 impl Session {
     pub fn new(stream: TcpStream, hostname: String) -> Self {
         Self {
-            stream, hostname
+            stream, hostname, state: SessionState::Created
         }
     }
 
@@ -30,11 +51,80 @@ impl Session {
 
         let (read, mut write) = self.stream.split();
         let mut reader = BufReader::new(read);
-        let mut state = SessionState::Connected;
+        self.state = SessionState::Connected;
         let mut line = String::new();
 
         send(&mut write, 
             &format!("220 {} ESMTP Ready", self.hostname)
-        ).await
+        ).await;
+
+        loop {
+            line.clear();
+            match reader.read_line(&mut line).await {
+                Ok(0) => {
+                    println!("Connection closed by {}", peer);
+                    break;
+                }
+                Err(e) => {
+                    println!("Read error from {}", peer);
+                    break;
+                }
+                Ok(_) => {}
+            }
+
+            let trimmed = line.trim();
+            println!("C: {}", trimmed);
+
+            if line.starts_with("HELO") {
+                let (cmd, host)= line.split_once(" ").expect("No host name");
+                send(&mut write,
+                    &format!("Hello {}, I am glad to meet you", host)
+                ).await;
+                continue;
+            }
+
+            if line.starts_with("MAIL FROM:") {
+                let f = line.clone();
+                let sender = f.chars()
+                    .skip(10)
+                    .filter(|c| *c == '<' || *c == '>')
+                    .collect();
+
+                self.state = SessionState::MailFrom(sender);
+                send(&mut write, "250 OK").await;
+                println!("Received Sender");
+                continue;
+            }
+
+            if line.starts_with("RCPT TO:") {
+                if let SessionState::MailFrom(sender) = &self.state  {
+                    let f = line.clone();
+                    let receiver = f.chars()
+                        .skip(8)
+                        .filter(|c| *c == '<' || *c == '>')
+                        .collect();
+                    self.state = SessionState::RcptTo(sender.clone(), receiver);
+                    send(&mut write, "250 OK").await;
+                    println!("Recieved recipient");
+                    continue;
+                }
+            }
+
+            if line.starts_with("DATA") {
+                if let SessionState::RcptTo(sender, recipient) = &self.state {
+                    self.state = SessionState::Data(sender.clone(), recipient.clone());
+                    println!("{} is now sending data", peer);
+                    send(&mut write, "354 End data with <CR><LF>.<CR><LF>").await;
+                    continue;
+                }
+            }
+            if line.ends_with("\r\n") {
+                if let SessionState::Data(sender, recipient) = &self.state {
+                    self.state = SessionState::Connected;
+                    println!("{} finished sending mail", peer);
+                    send(&mut write, "250 OK").await;
+                }
+            }
+        }
     }
 }
--- test.py
+++ test.py
@@ -3,18 +3,50 @@ import socket
 host = "127.0.0.1" 
 port = 2525
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.connect((host, port))
-
+def get_from_sock(sock):
     buf = b""
     while not buf.endswith(b"\r\n"):
         buf += sock.recv(1)
+
+    return buf.decode().strip()
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect((host, port))
+
+    line = get_from_sock(sock)
     
-    line = buf.decode().strip()
     print(f"S: {line}")
 
     if not line.startswith("220"):
         print("ERROR: Line didn't start with 220")
     
-    sock.send(b"HELO crepes.fr")
+    sock.send(b"HELO crepes.fr\r\n")
+    line = get_from_sock(sock)
+    print(line)
+
+    sock.send(b"MAIL FROM:<bob@example.org>\r\n")
+    line = get_from_sock(sock)
+    print(line)
+
+    sock.send(b"RCPT TO:<alice@example.com>\r\n")
+    line = get_from_sock(sock)
+    print(line)
+
+    sock.send(b"DATA\r\n")
+    line = get_from_sock(sock)
+    print(line)
+    print('=====DATA=====')
+
+    sock.send(b"I love France")
+    sock.send(b"\r\n")
+    line = get_from_sock(sock)
+    print(line)
+
+    print("DONE")
+
+
+    
+
+
 
