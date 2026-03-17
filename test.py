46403793ec1328429c824063360ac3ed88629bdc
W. Santos <w.santos.gg@gmail.com>
2026-03-17 12:58:53 +0800

    feat: add more stuff to test.py

--- test.py
+++ test.py
@@ -12,3 +12,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
     line = buf.decode().strip()
     print(f"S: {line}")
+
+    if not line.startswith("220"):
+        print("ERROR: Line didn't start with 220")
+    
+    sock.send(b"HELO crepes.fr")
+
